a580889f38fd68670e6f978054461d99a2b92786
rborder <border.richard@gmail.com>
2023-03-05 17:54:16 -0800

    added founders module

--- xftsim/__init__.py
+++ xftsim/__init__.py
@@ -5,6 +5,7 @@ from . import effect      ## genetic effects
 from . import arch        ## phenogenetic architectures
 from . import mate        ## mate assignment
 from . import reproduce   ## sexual reproduction and phenotypic transmission
+from . import founders    ## creation / import of founder haplotypes
 from . import sim         ## simulation object
 from . import stats       ## estimation
 from . import proc        ## post-processing
