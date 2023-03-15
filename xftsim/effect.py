03208d607a75b6052772bc94d7eedbb6862e2cd4
rborder <border.richard@gmail.com>
2023-03-15 12:47:33 -0700

    making scaled/std language sensible

--- xftsim/effect.py
+++ xftsim/effect.py
@@ -14,42 +14,65 @@ import xftsim as xft
 ## simple additive effects that are standardized and/or scaled
 class AdditiveEffects:
     def __init__(self,
-        beta: NDArray[Any, Any], ## effects matrix or vector
-        variant_indexer: xft.index.HaploidVariantIndex = None,
+        beta: NDArray[Any, Any], ## diploid effects matrix or vector
+        variant_indexer: Union[xft.index.HaploidVariantIndex, xft.index.DiploidVariantIndex] = None,
         component_indexer: xft.index.ComponentIndex = None,
-        standardized: bool = False, ## have the effects been divided by variant SD under HWE?
-        scaled: bool = False, ## have the effects been divided by the number of causal variants per column?
-
+        standardized: bool = True, ## True implies these are effects of standardized variants, False = Raw
+        scaled: bool = False, ## True implies these are effects of variants / sqrt(m_causal)
         ):
         
-        self.component_indexer = component_indexer
+        unscaled = not scaled
+        unstandardized = not standardized
+        if isinstance(variant_indexer, xft.index.HaploidVariantIndex):
+            self.component_indexer = component_indexer
+
         self.variant_indexer = variant_indexer
 
         self._tiledAF = xft.utils.ensure2D(self.variant_indexer.af)
         self.AF = xft.utils.ensure2D(self.variant_indexer.af)[::2,:]
         if np.any(np.isnan(self.AF)):
             raise RuntimeError('Must provided allele frequencies')
-        # self._tiledAF = np.tile(self.AF, 2)
 
         ## ensure shape
-        self._beta = xft.utils.ensure2D(beta)   
-        self.m, self.c = beta.shape
-        self._standardized = standardized
-        self._scaled = scaled
+        beta = xft.utils.ensure2D(beta)   
+        m, c = beta.shape
+        self.m, self.c = variant_indexer.m, sample_indexer.k_phenotypes
+        if (self.m != m) or (self.c != c):
+            raise RuntimeError('beta and indexers nonconformable')  
+        self._beta = beta   
+
+        self._beta_scaled_standardized_diploid = None
+        self._beta_unscaled_standardized_diploid = None
+        self._beta_unscaled_unstandardized_diploid = None
+        self._beta_scaled_unstandardized_diploid = None
+
+        if scaled and standardized:
+            self._beta_scaled_standardized_diploid = self._beta
+        elif scaled and unstandardized:
+            self._beta_scaled_unstandardized_diploid = self._beta
+        elif unscaled and standardized:
+            self._beta_unscaled_standardized_diploid = self._beta
+        elif unscaled and unstandardized:
+            self._beta_unscaled_unstandardized_diploid = self._beta
+        else:
+            raise TypeError("standardized, scaled must be bool")
 
         if component_indexer is None:
             self.component_indexer = xft.index.ComponentIndex.range_index(c)
 
     @property
-    def m_causal(self): ## if not provided set to number of non-zero betas per phenotype
-        # if self._m_causal is None:
+    def m_causal(self): ## number of non-zero betas per phenotype
         return(np.sum(self._beta != 0, axis = 0))
-        # else:
-        # return self._m_causal
 
     @cached_property
-    def beta_standardized_scaled_diploid(self): ## multiply against standardized (0,1,2)/sqrt(m) genotypes
-        if self._standardized:
+    def beta_scaled_standardized_diploid(self): ## multiply against standardized (0,1,2)/sqrt(m) genotypes
+        if self._beta_scaled_standardized_diploid is not None:
+            return self._beta_scaled_standardized_diploid
+        else: 
+            return beta_scaled_standardized_diploid
+
+
+        self._standardized:
             if self._scaled:
                 return self._beta
             else:
@@ -59,11 +82,11 @@ class AdditiveEffects:
         else:
             return self._beta * np.sqrt(2*self.AF*(1-self.AF)*self.m_causal)
 
-    @cached_property
+    @property
     def beta_standardized_scaled_haploid(self): 
         return self.beta_standardized_scaled_diploid.repeat(2,axis=0)
 
-    @cached_property
+    @property
     def beta_raw_diploid(self): ## multiple against raw (0,1,2) genotypes and add offset
         if self._standardized:
             if self._scaled:
@@ -75,11 +98,11 @@ class AdditiveEffects:
         else:
             return self._beta 
     
-    @cached_property
+    @property
     def beta_raw_haploid(self):
         return self.beta_raw_diploid.repeat(2,axis=0)
 
-    @cached_property
+    @property
     def offset(self): ## add to raw multiple against raw (0,1,2) 
         return np.sum(-2*self.AF * self.beta_raw_diploid, axis = 0)
 
