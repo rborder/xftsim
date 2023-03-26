db649f061db65a12ac6efb7050caa0847885c931
rborder <border.richard@gmail.com>
2023-03-26 13:33:30 -0700

    added requirements

--- setup.py
+++ setup.py
@@ -2,8 +2,26 @@ from setuptools import setup, find_packages
 
 setup(
     name='xftsim',
+    
     version='0.0.0',
+    
+    author="Richard Border",
+    
+    author_email="border.richard@gmail.com",
+    
     packages=find_packages(include=[
         'xftsim', 'xftsim*',
-    ])
+    ]),
+    
+    install_requires = [
+    "networkx",
+    "nptyping",
+    "numba",
+    "numpy",
+    "pandas",
+    "scipy",
+    "sgkit",
+    "xarray",
+    ],
+
 )
