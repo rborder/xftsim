17d0e47f788c99be3ccd19463a2dee0fa0263d39
rborder <border.richard@gmail.com>
2023-03-29 16:39:02 -0700

    chatgpt docstrings

--- xftsim/io.py
+++ xftsim/io.py
@@ -16,6 +16,15 @@ import xftsim as xft
 
 @nb.njit
 def _genotypes_to_pseudo_haplotypes(genotypes):
+    """Converts genotype data to pseudo-haplotype data using random permutations.
+
+    Args:
+        genotypes (ndarray): An array of genotype data.
+
+    Returns:
+        ndarray: An array of pseudo-haplotypes data.
+
+    """
     haplotypes = np.zeros((genotypes.shape[0],genotypes.shape[1]*2), dtype=np.int8)
     zero_one = np.array([0,1], dtype=np.int8)
     one_one = np.array([1,1], dtype=np.int8)
@@ -30,6 +39,15 @@ def _genotypes_to_pseudo_haplotypes(genotypes):
 
 
 def genotypes_to_pseudo_haplotypes(x):
+    """Converts genotype data in an xarray DataArray to pseudo-haplotype data.
+
+    Args:
+        x (xr.DataArray): An xarray DataArray containing genotype data.
+
+    Returns:
+        xr.DataArray: An xarray DataArray containing pseudo-haplotype data.
+
+    """
     out = x[:,np.repeat(np.arange(x.shape[1]),2)]
     out.values = _genotypes_to_pseudo_haplotypes(x.values)
     return out
@@ -37,6 +55,16 @@ def genotypes_to_pseudo_haplotypes(x):
 
 
 def plink1_variant_index(ppxr: xr.DataArray) -> xft.index.DiploidVariantIndex:
+    """Create a DiploidVariantIndex object from a plink file DataArray generated 
+    by pandas_plink.
+
+    Args:
+        ppxr (xr.DataArray): An xarray DataArray representing a plink file.
+
+    Returns:
+        xft.index.DiploidVariantIndex: A DiploidVariantIndex object.
+
+    """
     if np.all(ppxr.cm==0):
         cm = np.full_like(ppxr.cm, fill_value=np.NaN)
     return xft.index.DiploidVariantIndex(
@@ -50,6 +78,16 @@ def plink1_variant_index(ppxr: xr.DataArray) -> xft.index.DiploidVariantIndex:
 
 def plink1_sample_index(ppxr: xr.DataArray, 
                         generation: int = 0) -> xft.index.SampleIndex:
+    """Create a SampleIndex object from a plink file DataArray generated 
+    by pandas_plink.
+
+    Args:
+        ppxr (xr.DataArray): An xarray DataArray representing a plink file.
+        generation (int, optional): The generation of the individuals. Defaults to 0.
+
+    Returns:
+        xft.index.SampleIndex A SampleIndex object.
+    """
     return xft.index.SampleIndex(
                                  iid = ppxr.iid.values.astype(str),
                                  fid = ppxr.fid.values.astype(str),
@@ -59,6 +97,18 @@ def plink1_sample_index(ppxr: xr.DataArray,
 
 
 def read_plink1_as_pseudohaplotypes(path: str):
+    """
+    Reads in PLINK 1 binary genotype data and returns a HaplotypeArray object containing pseudo-haplotypes.
+    
+    Args:
+        path (str): The file path to the PLINK 1 binary genotype data.
+    
+    Returns:
+        xft.struct.HaplotypeArray: A HaplotypeArray object containing pseudo-haplotypes.
+    
+    Raises:
+        ValueError: If the specified file path does not exist or is not in the expected format.
+    """
     delayed_plink = pp.read_plink1_bin(path).astype(np.int8)
     delayed_plink_dask = pp.read_plink(path)
     dip_variant_index = plink1_variant_index(delayed_plink)
@@ -78,26 +128,37 @@ def read_plink1_as_pseudohaplotypes(path: str):
 
 
 
-    vindex = hh.xft.get_variant_indexer().to_diploid()
-    sindex = hh.xft.get_sample_indexer()
-    data = da.asarray(hh.xft.to_diploid()).astype(np.float32)
-    data_array = xr.DataArray(data,
-                              dims=['sample','variant'],
-                              coords = dict(
-                                            sample  = ("sample", sindex.iid.astype(str)),
-                                            fid     = ("sample", sindex.fid.astype(str)),
-                                            gender  = ("sample", 2 - sindex.sex.astype(int)),
-                                            variant = ("variant", vindex.vid.astype(str)),
-                                            snp     = ("variant", vindex.vid.astype(str)),
-                                            chrom   = ("variant", vindex.chrom.astype(str)),
-                                            a0      = ("variant", vindex.zero_allele.astype(str)),
-                                            a1      = ("variant", vindex.one_allele.astype(str)),
-                                            )
-                              )
-    pp.write_plink1_bin(data_array, path, verbose=verbose)
+    # vindex = hh.xft.get_variant_indexer().to_diploid()
+    # sindex = hh.xft.get_sample_indexer()
+    # data = da.asarray(hh.xft.to_diploid()).astype(np.float32)
+    # data_array = xr.DataArray(data,
+    #                           dims=['sample','variant'],
+    #                           coords = dict(
+    #                                         sample  = ("sample", sindex.iid.astype(str)),
+    #                                         fid     = ("sample", sindex.fid.astype(str)),
+    #                                         gender  = ("sample", 2 - sindex.sex.astype(int)),
+    #                                         variant = ("variant", vindex.vid.astype(str)),
+    #                                         snp     = ("variant", vindex.vid.astype(str)),
+    #                                         chrom   = ("variant", vindex.chrom.astype(str)),
+    #                                         a0      = ("variant", vindex.zero_allele.astype(str)),
+    #                                         a1      = ("variant", vindex.one_allele.astype(str)),
+    #                                         )
+    #                           )
+    # pp.write_plink1_bin(data_array, path, verbose=verbose)
 
 
 def write_to_plink1(hh: xr.DataArray, path: str, verbose: bool = True):
+    """
+    Writes a DataArray to a PLINK 1 binary file.
+
+    Args:
+        hh (xr.DataArray): A DataArray containing the genotype data to write.
+        path (str): The path to the output PLINK file. The '.bed' extension will be added automatically.
+        verbose (bool, optional): Whether to print verbose output during writing. Defaults to True.
+
+    Returns:
+        None
+    """
     vindex = hh.xft.get_variant_indexer().to_diploid()
     sindex = hh.xft.get_sample_indexer()
     data = da.asarray(hh.xft.to_diploid()).astype(np.float32)
@@ -124,6 +185,18 @@ def load_haplotype_zarr(path: str,
                         compute: bool = True,
                         **kwargs,
                         ) -> xr.DataArray:
+    """
+    Load haplotype data from a Zarr store.
+
+    Args:
+        path (str): The path to the Zarr store.
+        compute (bool): Whether to compute the data immediately. Defaults to True.
+        **kwargs: Additional keyword arguments to pass to xr.open_dataset().
+
+    Returns:
+        xr.DataArray: The loaded haplotype data as a DataArray.
+
+    """
     if compute:
         with ProgressBar():
             return xr.open_dataset(path,engine='zarr', **kwargs).HaplotypeArray.compute()
@@ -133,6 +206,17 @@ def load_haplotype_zarr(path: str,
 def save_haplotype_zarr(haplotypes: xr.DataArray,
                         path: str,
                         **kwargs,
-
                         ) -> None:
+    """
+    Save haplotype data to a Zarr store.
+
+    Args:
+        haplotypes (xr.DataArray): The haplotype data to save.
+        path (str): The path to the Zarr store.
+        **kwargs: Additional keyword arguments to pass to haplotypes.to_dataset().to_zarr().
+
+    Returns:
+        None
+
+    """
     haplotypes.to_dataset().to_zarr(path, **kwargs)
