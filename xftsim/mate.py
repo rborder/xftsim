761a7d657716f283e8a37b8753ef13b5207f6303
rborder <border.richard@gmail.com>
2023-06-30 16:07:47 -0700

    fixed ls callback

--- xftsim/mate.py
+++ xftsim/mate.py
@@ -1038,11 +1038,11 @@ def _solve_qap_ls(Y, Z, R, nb_threads=6, time_limit=30, tolerance=1e-5,
             if obj.value > self.last_best_value:
                 self.last_best_running_time = stats.running_time
                 self.last_best_value = obj.value
-            if stats.running_time - self.last_best_running_time > self.interval:
-                print(f">>>>>>> No improvement during {self.interval} seconds: resolution is stopped")
+            elif stats.running_time - self.last_best_running_time > self.interval:
+                print(f">>> No improvement during {self.interval} seconds: resolution is stopped")
                 ls.stop()
             else:
-                print(">>>>>> Objective %d" % obj.value)
+                pass #print(">>>>>> Objective %d" % obj.value)
     n = Y.shape[0]
     # for later use as initial value
     tmp = np.argsort(np.apply_along_axis(np.mean, 1, Y))[
